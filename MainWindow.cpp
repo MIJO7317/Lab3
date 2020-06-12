a0592427f27fb4af94e289b901068c43542aab78
Стас Ладыгин <ladygin_sa_1017@1511.ru>
2020-06-13 02:02:12 +0300

    Create BinarySearchTree

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,19 +1,47 @@
-#include "MainWindow.h"
+﻿#include "MainWindow.h"
 #include"BinarySearchTree.h"
 #include<functional>
+#include<complex>
+//#include"vld.h"
+
+namespace std
+{
+	//Перегрузка operator<< для возможности вывода в строку
+	template<typename T>
+	std::ostream& operator<<(std::ostream& out, const std::complex<T>& comp)
+	{
+		out << "(" << comp.real() << ", " << comp.imag() << ")";
+		return out;
+	}
+
+	//Перегрузка у класса std::hash operator() для взятия хэша. 
+	//Объединение двух хэшей происходит путем побитового сдвига и взятия операции XOR. 
+	//Так же есть возможность скомбинировать два хэш значения с помощью функции has_combine из библиотеки boost. 
+	template<typename T>
+	struct hash<std::complex<T>>
+	{
+		std::size_t operator()(const std::complex<T>& comp) const
+		{
+			std::size_t h1 = hash<T>{}(comp.real());
+			std::size_t h2 = hash<T>{}(comp.imag());
+			return h1 ^ (h2 << 1);
+		}
+	};
+}
 
 MainWindow::MainWindow(QWidget* parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-	BinarySearchTree<int> bst;
-	bst.Add(1);
-	bst.Add(2);
-	bst.Add(3);
-	bst.Add(4);
-	bst.Add(8);
-	bst.Add(17);
-	bst.Add(4);
-	bool a = bst.Find(9);
-	bool b = bst.Find(4);
+	BinarySearchTree<std::complex<double>> bst;
+	std::complex<double> comp = { 1,2 };
+	bst.SetRound(bst.LNR);
+	for (size_t i = 0; i < 8; i++)
+		bst.Add(std::complex<double>(rand()%100, rand() % 100));
+	bst.Add(comp);
+	bst.Balancing();
+	auto a = bst.Find(comp);
+	auto b = bst.Find((4,1));
+	bst.Delete(comp);
+	ui.lineEdit->setText(bst.ToString().c_str());
 }
